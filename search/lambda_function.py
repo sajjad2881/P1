bbb0ae3e1c3fe4fd9df238987b3a5e69c3b68f96
Sajjad Zafar <73673391+sajjad2881@users.noreply.github.com>
2023-04-02 12:58:18 -0400

    Update lambda_function.py

--- search/lambda_function.py
+++ search/lambda_function.py
@@ -3,7 +3,7 @@ import json
 import logging
 import boto3
 #from opensearchpy import OpenSearch, RequestsHttpConnection
-#Test comment
+#Test comment 2
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import inflection
